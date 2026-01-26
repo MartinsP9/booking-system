0f8be1bf419684055b80489e0a1eb16cfb46390f
Martins Piternieks <marcispit@gmail.com>
2026-01-26 22:50:48 +0200

    some styling to the services

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -15,33 +15,47 @@ const Service = () => {
             setActiveIndex(id);
         }
     };
+    //  someting to sort by services...
+    //     const rawServices = services;
+    //     let usableServices = rawServices.filter((service) => {
+    //         if (service.title.includes("Hair") || service.title.includes("hair")) {
+    //
+    //         }
+    //
+    //     });
 
     return (
-        <main className="flex w-full flex-col items-center justify-center py-20 bg-white dark:bg-black sm:items-start">
-            <div className="m-auto">
+        <main className="flex w-full flex-col items-center justify-center py-20 bg-white dark:bg-neutral-950 sm:items-start">
+            {/* <div className="m-auto">
                 <h1>Haircut</h1>
                 <p>Classic or modern look depending on your needs and requests.</p>
             </div>
             <div className="m-auto">
                 <h1>Beard trim</h1>
                 <p>Razor beard trim using hot and cold towels.</p>
+            </div> */}
+            <div className="max-w-150 flex flex-col justify-center bg-neutral-100 dark:bg-neutral-900 m-auto p-10 rounded-xl">
+                <div className="p-5">
+                    <h1 className="text-3xl text-black dark:text-white">Which service are you looking to book?</h1>
+                </div>
+                <div className="border-t border-gray-200 mb-6"></div>
+                <div className="m-auto flex flex-col gap-20">
+                    {services.map((card, index: number) => (
+                        <ServiceCard
+                            key={index}
+                            id={card.id}
+                            onCardClick={() => chooseThis(card.id)}
+                            title={card.title}
+                            text={card.text}
+                            image={card.image}
+                            imagealt={card.imagealt}
+                            price={card.price}
+                            isActive={activeIndex == card.id}
+                        />
+                    ))}
+                </div>
+                <Footer serviceId={activeIndex} />
             </div>
-            <div className="m-auto flex flex-col gap-20">
-                {services.map((card, index: number) => (
-                    <ServiceCard
-                        key={index}
-                        id={card.id}
-                        onCardClick={() => chooseThis(card.id)}
-                        title={card.title}
-                        text={card.text}
-                        image={card.image}
-                        imagealt={card.imagealt}
-                        price={card.price}
-                        isActive={activeIndex == card.id}
-                    />
-                ))}
-            </div>
-            <Footer serviceId={activeIndex} />
         </main>
     );
 };
